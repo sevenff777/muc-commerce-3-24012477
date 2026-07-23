3307cdd5148bcfda95b7b41458834900c0c7b5f9
renxian wang <2556382119@qq.com>
2026-07-23 16:06:38 +0800

    Delete day07_24012477_王仁献 directory

--- day07_24012477_王仁献/tmp_verify_metrics.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-from services.data_service import load_dashboard_data
-
-data = load_dashboard_data(Path('.'))
-for metric in data['metrics']:
-    print(metric['label'], metric['value'], metric['note'])
